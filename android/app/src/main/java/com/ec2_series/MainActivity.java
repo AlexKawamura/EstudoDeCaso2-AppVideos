72687658ad785785dee766992f25a574f10a7196
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-08-28 09:15:03 -0300

    Criando projeto react-native 0.60.0

---
+++ android/app/src/main/java/com/ec2_series/MainActivity.java
@@ -0,0 +1,15 @@
+package com.ec2_series;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "EC2_Series";
+    }
+}
