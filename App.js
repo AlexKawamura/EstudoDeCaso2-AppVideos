52ef6fd4954d9bde73db43cffd495526fcc81d14
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-02 08:38:29 -0300

    Estilizando header das páginas

--- App.js
+++ App.js
@@ -1,10 +1,27 @@
-import { createAppContainer, createStackNavigator } from "react-navigation";
-import Login from "./src/pages/Login";
+import { createAppContainer, createStackNavigator } from 'react-navigation';
+import Login from './src/pages/Login';
 
 const AppNavigator = createStackNavigator({
   Login: {
     screen: Login,
+    navigationOptions: {
+      title: 'Bem-vindo',
+    },
   },
+}, {
+    defaultNavigationOptions: {
+      title: 'Minhas series',
+      headerTintColor: '#FFF',
+      headerStyle: {
+        backgroundColor: '#003994',
+        borderBottomWidth: 1,
+        borderBottomColor: '#C5C5C5',
+      },
+      headerTitleStyle: {
+        color: '#FFF',
+        fontSize: 30,
+      },
+    },
 });
 
 const AppContainer = createAppContainer(AppNavigator);
