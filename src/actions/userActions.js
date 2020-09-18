e1b3c9d8a5eeeba8751a0913d753a0eeff524504
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-18 20:24:05 -0300

    Disparando action userLoginSuccess ao termino da chamada assincrona do firebase

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,21 +1,24 @@
-const USER_LOGIN_SUCCESS = 'USER_LOGIN';
+import firebase from 'firebase';
+
+export const USER_LOGIN_SUCCESS = 'USER_LOGIN';
 const userLoginSuccess = user => ({
   type: USER_LOGIN_SUCCESS,
   user,
 });
 
-const USER_LOGOUT = 'USER_LOGOUT';
+export const USER_LOGOUT = 'USER_LOGOUT';
 const userLogout = () => ({
   type: USER_LOGOUT,
 });
 
-export const processLogin = ({email, password}) => {
+export const processLogin = ({email, password}) => dispatch => {
   firebase.auth()
     .signInWithEmailAndPassword(email, password)
-    .then(
-
-    )
-    .catch(error => {
+    .then( user => {
+      const action = userLoginSuccess(user);
+      dispatch(action);
+    });
+    /*.catch(error => {
       if (error.code === 'auth/user-not-found') {
         Alert.alert(
           'Usuário não encontrado',
@@ -40,5 +43,5 @@ export const processLogin = ({email, password}) => {
       loginUserFail;
     }).then(() => {
       this.setState({ isLoading: false });
-    });
-}
+    });*/
+};
