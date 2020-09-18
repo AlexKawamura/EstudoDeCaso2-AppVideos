0a9319c731ef2b730f98ac679afab2e8b0120da2
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-18 18:39:56 -0300

    Começando a utilizar react-redux e criando store

---
+++ src/SeriesApp.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Router from './Router';
+
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './reducers';
+
+const store = createStore(rootReducer);
+
+const SeriesApp = prop => (
+  <Provider store={store}>
+    <Router />
+  </Provider>
+);
+
+export default SeriesApp;
