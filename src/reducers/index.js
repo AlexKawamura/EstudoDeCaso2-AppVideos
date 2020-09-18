d93791a8b66103c7d9d6fe82ad07a79b4792af77
ALEXANDRE KAWAMURA TANAKA <alexandre.k.tanaka@gmail.com>
2020-09-18 19:35:43 -0300

    Criando userReducers para executar as actions relacionadas ao user

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,6 @@
 import { combineReducers } from 'redux';
+import userReducers from './userReducer';
 
 export default combineReducers({
-  qualquer: (state = [], action) => state,
+  user: userReducers,
 });
